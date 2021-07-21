f7d293668328a45ab70d779aaf3b93ff0b1f3d2c
Kevin Doerr <doerrkev@msu.edu>
2021-07-21 11:27:22 -0400

    Added ui_app.py file

--- src/myApp/ui/ui_app.py
+++ src/myApp/ui/ui_app.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_app.ui'
 #
-# Created by: PyQt5 UI code generator 5.14.1
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,16 +12,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1024, 768)
+        MainWindow.resize(1284, 709)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/icons/default.png"),
-                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/default.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName("verticalLayout")
         self.matplotlibbaseWidget = MatplotlibBaseWidget(self.centralwidget)
+        self.matplotlibbaseWidget.setGeometry(QtCore.QRect(0, 0, 640, 480))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.matplotlibbaseWidget.sizePolicy().hasHeightForWidth())
+        self.matplotlibbaseWidget.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("sans-serif")
         font.setPointSize(14)
@@ -43,11 +50,10 @@ class Ui_MainWindow(object):
         font.setItalic(False)
         font.setWeight(50)
         self.matplotlibbaseWidget.setFigureXYticksFont(font)
+        self.matplotlibbaseWidget.setProperty("figureToolbarToggle", False)
         self.matplotlibbaseWidget.setObjectName("matplotlibbaseWidget")
-        self.verticalLayout.addWidget(self.matplotlibbaseWidget)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
         self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 480, 121, 19))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -56,11 +62,11 @@ class Ui_MainWindow(object):
             self.label.sizePolicy().hasHeightForWidth())
         self.label.setSizePolicy(sizePolicy)
         self.label.setObjectName("label")
-        self.horizontalLayout.addWidget(self.label)
         self.quad1_name_cbb = QtWidgets.QComboBox(self.centralwidget)
+        self.quad1_name_cbb.setGeometry(QtCore.QRect(120, 480, 79, 27))
         self.quad1_name_cbb.setObjectName("quad1_name_cbb")
-        self.horizontalLayout.addWidget(self.quad1_name_cbb)
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(210, 480, 111, 19))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -69,15 +75,45 @@ class Ui_MainWindow(object):
             self.label_2.sizePolicy().hasHeightForWidth())
         self.label_2.setSizePolicy(sizePolicy)
         self.label_2.setObjectName("label_2")
-        self.horizontalLayout.addWidget(self.label_2)
         self.quad1_grad_dsbox = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.quad1_grad_dsbox.setGeometry(QtCore.QRect(320, 480, 111, 28))
         self.quad1_grad_dsbox.setDecimals(3)
         self.quad1_grad_dsbox.setObjectName("quad1_grad_dsbox")
-        self.horizontalLayout.addWidget(self.quad1_grad_dsbox)
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.matplotlibbaseWidget_1 = MatplotlibBaseWidget(self.centralwidget)
+        self.matplotlibbaseWidget_1.setGeometry(QtCore.QRect(640, 0, 640, 480))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.matplotlibbaseWidget_1.sizePolicy().hasHeightForWidth())
+        self.matplotlibbaseWidget_1.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setFamily("sans-serif")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.matplotlibbaseWidget_1.setFigureXYlabelFont(font)
+        font = QtGui.QFont()
+        font.setFamily("sans-serif")
+        font.setPointSize(16)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.matplotlibbaseWidget_1.setFigureTitleFont(font)
+        font = QtGui.QFont()
+        font.setFamily("sans-serif")
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.matplotlibbaseWidget_1.setFigureXYticksFont(font)
+        self.matplotlibbaseWidget_1.setProperty("figureToolbarToggle", False)
+        self.matplotlibbaseWidget_1.setObjectName("matplotlibbaseWidget_1")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1024, 30))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1284, 24))
         self.menubar.setObjectName("menubar")
         self.menu_File = QtWidgets.QMenu(self.menubar)
         self.menu_File.setObjectName("menu_File")
@@ -109,9 +145,17 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.matplotlibbaseWidget.setFigureXlabel(
+            _translate("MainWindow", "X (mm)"))
+        self.matplotlibbaseWidget.setFigureYlabel(
+            _translate("MainWindow", "X\' (mrad)"))
         self.label.setText(_translate("MainWindow", "Select quadrupole"))
         self.label_2.setText(_translate("MainWindow", "Gradient Setting"))
         self.quad1_grad_dsbox.setSuffix(_translate("MainWindow", " T/m"))
+        self.matplotlibbaseWidget_1.setFigureXlabel(
+            _translate("MainWindow", "Y (mm)"))
+        self.matplotlibbaseWidget_1.setFigureYlabel(
+            _translate("MainWindow", "Y\' (mrad)"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
         self.action_About.setText(_translate("MainWindow", "&About"))
